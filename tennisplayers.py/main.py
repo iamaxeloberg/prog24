5169e2f5939783aadad4b1d87c286d73aaffc5e0
iamaxeloberg <stadium004@gmail.com>
2024-11-05 14:29:23 +0100

    fler funktoin
    jabababa

--- tennisplayers.py/main.py
+++ tennisplayers.py/main.py
@@ -5,3 +5,16 @@ class Player:
         self.serve_win_prob = serve_win_prob
         self.wins = wins
         self.matches = matches
+
+    def win_percentage(self):
+        if self.matches > 0 :
+            return self.wins / self.matches
+        else:
+            return "N/A"
+        
+    def uppdated_result(self, won):
+        self.matches += 1
+        if won:
+            self.wins += 1
+    
+def read_stats_from_file(FILNAMN):
